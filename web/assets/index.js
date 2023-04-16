8472959f6f62e0aca83d80cd235d7f1ae40a13c5
Omar Brikaa <brikaaomar@gmail.com>
2023-04-16 23:04:51 +0200

    Initial UI

--- web/assets/index.js
+++ web/assets/index.js
@@ -1,4 +1,12 @@
 (async () => {
+    const createImage = (src, width, height) => {
+        const image = document.createElement('img');
+        image.setAttribute('src', src);
+        image.setAttribute('width', width);
+        image.setAttribute('height', height);
+        return image;
+    };
+
     const form = document.getElementsByTagName('form')[0];
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -27,6 +35,45 @@
         const response = await (
             await fetch('http://localhost:2005/uninformed', requestOptions)
         ).json();
-        console.log(response);
+
+        const results = response.results;
+        const resultsContainer = document.getElementById('resultsContainer');
+
+        const IMAGE_WIDTH = 50;
+        const IMAGE_HEIGHT = 50;
+
+        results.forEach((result) => {
+            const table = document.createElement('table');
+            resultsContainer.appendChild(table);
+
+            result.forEach((row) => {
+                const tableRow = document.createElement('tr');
+                table.appendChild(tableRow);
+                row.filter((cell) => cell !== 'd' && cell !== 'r').forEach((cell) => {
+                    const tableCell = document.createElement('td');
+                    if (cell === 'u') {
+                        tableCell.setAttribute('rowspan', 2);
+                        tableCell.appendChild(
+                            createImage('/assets/domino-vertical.svg', IMAGE_WIDTH, IMAGE_HEIGHT * 2)
+                        );
+                    }
+                    if (cell === 'l') {
+                        tableCell.setAttribute('colspan', 2);
+                        tableCell.appendChild(
+                            createImage('/assets/domino-horizontal.svg', IMAGE_WIDTH * 2, IMAGE_HEIGHT)
+                        );
+                    }
+                    if (cell === 'b') {
+                        tableCell.appendChild(
+                            createImage('/assets/bomb.svg', IMAGE_WIDTH, IMAGE_HEIGHT)
+                        );
+                    }
+                    tableRow.appendChild(tableCell);
+                });
+            });
+
+            resultsContainer.appendChild(document.createElement('br'));
+            resultsContainer.appendChild(document.createElement('br'));
+        });
     });
 })();
