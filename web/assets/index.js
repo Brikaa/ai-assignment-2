35b6c18e59280ddce71e207e60e3ddc74aeabe91
Omar Brikaa <brikaaomar@gmail.com>
2023-04-16 22:22:42 +0200

    Input form

--- web/assets/index.js
+++ web/assets/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+(async () => {
+    const form = document.getElementsByTagName('form')[0];
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const formData = Object.fromEntries(new FormData(form));
+        const requestBody = {
+            rows: parseInt(formData.rows),
+            columns: parseInt(formData.columns),
+            bomb1: {
+                row: parseInt(formData.bomb1Row),
+                column: parseInt(formData.bomb1Column)
+            },
+            bomb2: {
+                row: parseInt(formData.bomb2Row),
+                column: parseInt(formData.bomb2Column)
+            }
+        };
+
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        const raw = JSON.stringify(requestBody);
+        const requestOptions = {
+            method: 'POST',
+            headers,
+            body: raw
+        };
+        const response = await (
+            await fetch('http://localhost:2005/uninformed', requestOptions)
+        ).json();
+        console.log(response);
+    });
+})();
